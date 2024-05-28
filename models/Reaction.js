cf57a07f227d9923eb35c10458f0e1f8df14813d
Alexis Merino <alexismerino49@gmail.com>
2024-05-27 20:43:31 -0500

    started the reaction schema

---
+++ models/Reaction.js
@@ -0,0 +1,10 @@
+// importing the dependecies schema and types from mongoose library
+const {Schema, Types} = reuqire("mongoose");
+
+// creating the reaction schema
+const reactionSchema = new Schema()
+
+
+
+//exports the reaction schema
+module.exports = reactionSchema;
