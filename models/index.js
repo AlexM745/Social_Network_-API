7d9a52565ef56b73bbad75c445d0f6563eaf4bd0
Alexis Merino <alexismerino49@gmail.com>
2024-05-28 19:32:25 -0500

    added models user and thought

---
+++ models/index.js
@@ -0,0 +1,6 @@
+// importing the models for user and thought
+const User = require("./User");
+const Thought = require ("./Thought");
+
+//exports te models for user and thought
+module.exports = {Thought, User};
