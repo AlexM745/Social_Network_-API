801d6b44265a32752e4fc82e1e5ae1314ecba4d3
Alexis Merino <alexismerino49@gmail.com>
2024-05-30 18:48:33 -0500

    started the usercontroller code

--- server.js
+++ server.js
@@ -6,5 +6,8 @@ const routes = require("./routes");
 // setting up the varibles for the port and app (enviroment variables)
 
 const PORT = process.env.PORT || 3001;
-const app = express()
+const app = express();
+
+
+
 
