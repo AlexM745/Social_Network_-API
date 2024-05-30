c15b05b22d5628c05a8a61921729381471e88c19
Alexis Merino <alexismerino49@gmail.com>
2024-05-30 18:42:30 -0500

    added file server.js

---
+++ server.js
@@ -0,0 +1,10 @@
+// importing the neccesary packages and file
+const express = require("express");
+const db = require("./config/connection");
+const routes = require("./routes");
+
+// setting up the varibles for the port and app (enviroment variables)
+
+const PORT = process.env.PORT || 3001;
+const app = express()
+
