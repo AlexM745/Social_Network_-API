9e90e440f08b0532f3c6719da0c942b78a502b2b
Alexis Merino <alexismerino49@gmail.com>
2024-05-30 22:52:26 -0500

    added the create thought handler

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -18,6 +18,20 @@ const ThoughtController = {
     async getThought(req, res) {
         try {
             const thoughtData = await Thought.findOne({ _id: req.params.courseId });
+            
+            if (!thoughtData) {
+                res.status(404).json({ message: "No thoughts found with this id!" });
+            }
+            res.status(200).json(thoughtData);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+
+    // create thought
+    async createThought(req, res) {
+        try {
+            const thoughtData = await Thought.create(req.body);
             res.status(200).json(thoughtData);;
         } catch (err) {
             res.status(500).json(err);
