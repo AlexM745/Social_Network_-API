9899da8a4db6545c37c97b84ea3d958d576c00e8
Alexis Merino <alexismerino49@gmail.com>
2024-05-30 21:24:11 -0500

    added the getThoughts handler

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -5,5 +5,13 @@ const {Types} = require("mongoose");
 
 // the thought controller object that has all the handlers for the api calls
 const ThoughtController = {
-  
+   // get all thoughts
+   async getThoughts(req, res) {
+    try {
+        const thoughtData = await Thought.find();
+        res.status(200).json(thoughtData);;
+    } catch (err) {
+        res.status(500).json(err);
+    }
+},
 }
