b90d3c88b8152679e7c98a2e9cde7a4f1eabe5f7
Alexis Merino <alexismerino49@gmail.com>
2024-05-31 01:22:25 -0500

    finished the server.js

--- server.js
+++ server.js
@@ -8,6 +8,18 @@ const routes = require("./routes");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+//use middleware to parse the data
+app.use(express.urlencoded ({extended:true}));
+app.use(express.json());
 
+// use the routes tha come from the routes.js
 
+app.use(routes);
 
+// connects to mongoDB and starts the server
+
+db.once("open", () => {
+    app.listen(PORT, () => {
+        console.log(`API server forrunning on port ${PORT}!`);
+    })
+})
