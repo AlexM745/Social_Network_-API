ffea056a4b0086221e1cf862341c29ead21911eb
Alexis Merino <alexismerino49@gmail.com>
2024-06-02 17:14:10 -0500

    finished the index.js in routes folder

--- routes/index.js
+++ routes/index.js
@@ -1,2 +1,15 @@
 // importing thr router dependecy from express library
 const router = require("express").Router();
+
+//importing the user and thought routes through the api folder
+const apiRoutes = require("./api");
+
+// defining the routes for the api
+router.use("/api", apiRoutes);
+
+router.use((req, res) => {
+    return res.status(404).send("Not found");
+});
+
+//exporting the router
+module.exports = router;
